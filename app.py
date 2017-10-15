8adac4290440726bbc688934df2de6915433073c
s0yabean <tszhin07@hotmail.com>
2017-10-15 00:21:17 -0700

    using my own code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from datetime import datetime
 
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 
 ######################################
